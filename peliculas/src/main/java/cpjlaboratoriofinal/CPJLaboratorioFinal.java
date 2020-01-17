8e4603c73986090daffb5de2407b27b71b869477
Emiliano <emiliano.guido@gmail.com>
2020-01-17 18:35:54 -0300

    Creado el 16/01 a las 23:32

---
+++ peliculas/src/main/java/cpjlaboratoriofinal/CPJLaboratorioFinal.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cpjlaboratoriofinal;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Emiliano
+ */
+public class CPJLaboratorioFinal {
+    
+    private static final Scanner scanner = new Scanner(System.in);
+    private static int opcion = -1;
+    private static final String nombreArchivo = "c:\\";
+    
+    
+}
